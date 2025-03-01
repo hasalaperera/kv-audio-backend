e7c486e10ddc3933bb0b8f8e517b42996f329443
hasalaperera <hasala294@gmail.com>
2025-03-01 21:29:57 +0530

    inquiry completed 🎉🎉

--- controllers/inquiryController.js
+++ controllers/inquiryController.js
@@ -1,42 +1,149 @@
 import Inquiry from "../models/inquiry.js";
-import { isItCustomer } from "./userController.js";
+import { isITAdmin, isItCustomer } from "./userController.js";
 
-export async function addInquiry(req,res)
-{
-   try{
-
+export async function addInquiry(req, res) {
+  try {
     if (isItCustomer(req)) {
-        const data = req.body
-        data.email = req.user.email
-        data.phone = req.user.phone
+      const data = req.body;
+      data.email = req.user.email;
+      data.phone = req.user.phone;
 
-        // id genarate
-        let id = 0;
+      // id genarate
+      let id = 0;
 
-        // get data from database and sort and limit
-        const inquiries = await Inquiry.find().sort({ id: -1 }).limit(1); 
+      // get data from database and sort and limit
+      const inquiries = await Inquiry.find().sort({ id: -1 }).limit(1);
 
-        if(inquiries.length == 0){
-            id = 1
-        }else{
-            id = inquiries[0].id + 1
-        }
+      if (inquiries.length == 0) {
+        id = 1;
+      } else {
+        id = inquiries[0].id + 1;
+      }
 
-        data.id = id
+      data.id = id;
 
-        const newInquiry = new Inquiry(data)
-        const response = await newInquiry.save()
+      const newInquiry = new Inquiry(data);
+      const response = await newInquiry.save();
 
-        res.json({
-            message : "Inquiry added successfully",
-            id : response.id
-        })
-
+      res.json({
+        message: "Inquiry added successfully",
+        id: response.id,
+      });
     }
-
-   } catch(e){
+  } catch (e) {
     res.status(500).json({
-        message: 'Error while adding inquiry',
-    })
-   }
+      message: "Error while adding inquiry",
+    });
+  }
+}
+
+export async function getInquiries(req, res) {
+  try {
+    if (isItCustomer(req)) {
+      const inquiries = await Inquiry.find({ email: req.user.email });
+      res.json(inquiries);
+      return;
+    } else if (isITAdmin(req)) {
+      const inquiries = await Inquiry.find();
+      res.json(inquiries);
+      return;
+    } else {
+      res.status(403).json({
+        message: "You are not authorized to access this resource",
+      });
+      return;
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "Error while getting inquiries",
+    });
+  }
+}
+
+export async function deleteInquiry(req, res) {
+  try {
+    if (isITAdmin(req)) {
+      const id = req.params.id;
+
+      await Inquiry.deleteOne({ id: id });
+      res.json({
+        message: "Inquiry deleted successfully",
+      });
+      return;
+    } else if (isItCustomer(req)) {
+      const id = req.params.id;
+
+      const inquiry = await Inquiry.findOne({ id: id });
+      if (inquiry == null) {
+        res.status(404).json({
+          message: "Inquiry not found",
+        });
+        return;
+      } else {
+        if (inquiry.email == req.user.email) {
+          await Inquiry.deleteOne({ id: id });
+          res.json({
+            message: "Inquiry deleted successfully",
+          });
+          return;
+        } else {
+          res.status(403).json({
+            message: "You are not authorized to delete this inquiry",
+          });
+          return;
+        }
+      }
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "Error while deleting inquiry",
+    });
+  }
+}
+
+export async function updateInquiry(req, res) {
+  try {
+    if (isITAdmin(req)) {
+      const id = req.params.id;
+      const data = req.body;
+
+      await Inquiry.updateOne({ id: id }, data);
+      res.json({
+        message: "Inquiry updated successfully",
+      });
+      return;
+    } else if (isItCustomer(req)) {
+      const id = req.params.id;
+      const data = req.body;
+
+      const inquiry = await Inquiry.findOne({ id: id });
+      if (inquiry == null) {
+        res.status(404).json({
+          message: "Inquiry not found",
+        });
+        return;
+      } else {
+        if (inquiry.email == req.user.email) {
+          await Inquiry.updateOne({ id: id }, { message: data.message });
+          res.json({
+            message: "Inquiry updated successfully",
+          });
+          return;
+        } else {
+          res.status(403).json({
+            message: "You are not authorized to update this inquiry",
+          });
+          return;
+        }
+      }
+    } else {
+      res.status(403).json({
+        message: "You are not authorized to update this inquiry",
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "Error while updating inquiry",
+    });
+  }
 }
--- models/inquiry.js
+++ models/inquiry.js
@@ -9,7 +9,6 @@ const inquirySchema = new mongoose.Schema({
     email : {
         type : String,
         required : true,
-        unique : true
     },
     message : {
         type : String,
@@ -36,6 +35,6 @@ const inquirySchema = new mongoose.Schema({
     }
 })
 
-const Inquiry = mongoose.model("Inquiry",inquirySchema);
+const Inquiry = mongoose.model("Inquiries",inquirySchema);
 
 export default Inquiry;
